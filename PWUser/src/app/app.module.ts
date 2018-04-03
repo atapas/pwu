89bbd76c3a5d1ecb2b0836fb4a9e1b834ab93557
Tapas Adhikary <tapas.adhikary@gmail.com>
2018-04-03 19:37:29 +0530

    Added Service Worker

--- PWUser/src/app/app.module.ts
+++ PWUser/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'; 
 
 import { MatToolbarModule, MatCardModule  } from '@angular/material';
 
@@ -18,7 +20,8 @@ import { UserCardService } from './user-card/user-card.service';
     BrowserModule,
     HttpClientModule,
     MatToolbarModule,
-    MatCardModule 
+    MatCardModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
